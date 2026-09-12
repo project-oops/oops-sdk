aa09ab1699cbb7c380f935cbad7a2420a0120f00
legboots <201123593+legboots@users.noreply.github.com>
2026-09-12 09:11:21 +0100

    agc: add DB_RENDER_OVERRIDE and late-Z shader control for linear depth buffers

--- src/agc/agc_draw.c
+++ src/agc/agc_draw.c
@@ -139,6 +139,8 @@ int oops_agc_draw_primitive(oops_gpu_queue_t *queue, const oops_agc_draw_desc_t 
       val = OOPS_AGC_CB_COLOR_ATTRIB2(desc->width, desc->height);
     } else if (reg == 0x200u) {
       val = (desc->depth_buffer && desc->depth_control) ? desc->depth_control : 0u;
+    } else if (reg == 0x203u) {
+      val = (desc->depth_buffer && desc->depth_control) ? 0x00000000u : 0x00000010u;
     } else if (reg == 0x10fu || reg == 0x110u) {
       val = float_as_u32(half_w);
     } else if (reg == 0x111u || reg == 0x112u) {
@@ -159,14 +161,15 @@ int oops_agc_draw_primitive(oops_gpu_queue_t *queue, const oops_agc_draw_desc_t 
       uint32_t reg;
       uint32_t val;
     } db_regs[] = {
-      {OOPS_AGC_REG_DB_RENDER_CONTROL, 0x00000040u}, /* DEPTH_COMPRESS_DISABLE */
-      {OOPS_AGC_REG_DB_DEPTH_VIEW,     0x00000000u},
-      {OOPS_AGC_REG_DB_DEPTH_SIZE_XY,  0},
-      {OOPS_AGC_REG_DB_Z_INFO,         0},
-      {OOPS_AGC_REG_DB_Z_READ_BASE,    0},
-      {OOPS_AGC_REG_DB_Z_READ_BASE_HI, 0},
-      {OOPS_AGC_REG_DB_Z_WRITE_BASE,   0},
-      {OOPS_AGC_REG_DB_Z_WRITE_BASE_HI,0},
+      {OOPS_AGC_REG_DB_RENDER_CONTROL,  0x00000060u}, /* DEPTH_COMPRESS_DISABLE | STENCIL_COMPRESS_DISABLE */
+      {OOPS_AGC_REG_DB_RENDER_OVERRIDE, 0x000003aau}, /* FORCE_HIZ_ENABLE=2, FAST_Z_DISABLE=1, FAST_STENCIL_DISABLE=1, NOOP_CULL_DISABLE=1 */
+      {OOPS_AGC_REG_DB_DEPTH_VIEW,      0x00000000u},
+      {OOPS_AGC_REG_DB_DEPTH_SIZE_XY,   0},
+      {OOPS_AGC_REG_DB_Z_INFO,          0},
+      {OOPS_AGC_REG_DB_Z_READ_BASE,     0},
+      {OOPS_AGC_REG_DB_Z_READ_BASE_HI,  0},
+      {OOPS_AGC_REG_DB_Z_WRITE_BASE,    0},
+      {OOPS_AGC_REG_DB_Z_WRITE_BASE_HI, 0},
     };
     for (size_t i = 0; i < sizeof(db_regs) / sizeof(db_regs[0]); i++) {
       uint32_t reg = db_regs[i].reg;
